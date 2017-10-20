f90fcbce633d0a2be1ab7301467481356048d0de
ev-dev <evan.reed1100@gmail.com>
2017-10-20 08:45:33 -0400

    Implementing dev-server and test server for graphql db

--- tests/dbTestServer.js
+++ tests/dbTestServer.js
@@ -1,7 +1,6 @@
 const path = require('path')
   , express = require('express')
   , bodyParser = require('body-parser')
-  , axios = require('axios')
   , { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
   , schema = require('./db/schema')
   , app = express()
@@ -10,35 +9,16 @@ app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
 
-// Logging Middleware
+  // Logging Middleware
   .use(require('volleyball'))
-  
-// All APIs With Cross-Origin Restraints
-  .use('/api', require('./api'))
-  
-// Serve Graphql DB
-  // .use('/gql', graphqlExpress({ schema }))
-  // .use('/graphiql', graphiqlExpress({
-  //   endpointURL: '/gql'
-  // }))
 
-// Serve React App
-  .get('/bundle.js', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'bundle.js'))
-  })
+  // Serve Graphql DB
+  .use('/gql', graphqlExpress({ schema }))
+  .use('/graphiql', graphiqlExpress({
+    endpointURL: '/gql'
+  }))
 
-// Serve Assets
-  .use((req, res, next) => {
-    if (path.extname(req.path).length > 0) res.status(404).end()
-    else next(null)
-  })
-
-// Serve HTML
-  .get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
-  })
-
-// Error Endware
+  // Error Endware
   .use((err, req, res, next) => {
     console.error(err, typeof next)
     console.error(err.stack)
@@ -46,13 +26,13 @@ app
   })
 
 
-const PORT = 3000
+const PORT = 5000
 const chalk = require('chalk')
 app.listen(PORT, () => {
-  const name = chalk.red.bold('[aggSci Server]')
+  const name = chalk.red.bold('[DB Test Server]')
   const url = chalk.cyan.bold(`http://localhost:`)
   const listen = chalk.yellow.bold('Listening')
-  
+
   console.log(`
   ${name} - ${listen} - ${url}${chalk.yellow(PORT)}
   `)
