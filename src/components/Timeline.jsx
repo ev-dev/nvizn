6803682e11a42c5e02e25ef5d1414d97acbfa47b
ev-dev <evan.reed1100@gmail.com>
2017-10-20 08:12:39 -0400

    Added scaffold for RotatingFields Profile and Timeline

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Timeline extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className=''>
+        
+      </div>
+    )
+  }
+}
+
+export default Timeline
